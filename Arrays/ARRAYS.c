f6dbe8fd9fc8c4e8692e159ac85fcc3bb8892489
Suhail0011 <154126179+Suhail0011@users.noreply.github.com>
2024-02-22 07:57:16 +0000

    Array Intro

---
+++ Arrays/ARRAYS.c
@@ -0,0 +1,11 @@
+// An Array is a collection of similar kind of data elements stored in adjacent memory locations and reffered to by a single array name. so if we want to store 100 vlues then we jus need declare one array mnot declare 100 varables.
+// Memory of an array is depedent on datatype of array if it is int the each value take 2 bytes
+// All element of an array share name and they are distinguished from one another with the help of index number.
+
+// Syntax of Array
+// int Arr1[size];   
+// where int is datatype arr1 is array mane and [Size] number of array elements.
+//The amount of storage for declare array has specified at compile time.
+
+
+
